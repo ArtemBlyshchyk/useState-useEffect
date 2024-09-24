50dde632de6d3eaf0b764fd662d914e1c6ef9fae
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-09-24 15:27:58 +0300

    Added the ability to save the feeddback in LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Description from "./components/Description/Description.jsx";
 import Feedback from "./components/Feedback/Feedback.jsx";
 import Options from "./components/Options/Options.jsx";
 import Notification from "./components/Notification/Notification.jsx";
 
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = window.localStorage.getItem("feedback");
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
   });
 
   const initialFeedback = {
@@ -52,6 +58,11 @@ function App() {
   const handleReset = () => {
     setFeedback(initialFeedback);
   };
+
+  //Saved the collected feedback to LocalStorage
+  useEffect(() => {
+    window.localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
 
   return (
     <>
